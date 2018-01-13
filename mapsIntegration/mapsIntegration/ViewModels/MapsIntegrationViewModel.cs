5fb54f424520c3c2653fb9203ed9142d9fb86dda
matheusnno <matheusnno@gmail.com>
2018-01-13 14:26:07 -0200

    mapsIntegration

---
+++ mapsIntegration/mapsIntegration/ViewModels/MapsIntegrationViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Xamarin.Forms.Maps;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace mapsIntegration.ViewModels
+{
+    public class mapsIntegrationViewModel : BaseViewModel
+    {
+
+        #region Buttons
+        public Command SateliteCommand { get; }
+        public Command RuaCommand { get; }
+        public Command HibridoCommand { get; }
+        #endregion
+
+        #region Variables
+        private MapType _type;
+        public MapType Type
+        {
+            get { return _type; }
+            set { SetProperty(ref _type, value); }
+        }
+        #endregion
+
+        public mapsIntegrationViewModel()
+        {
+            SateliteCommand = new Command(ExecuteSateliteCommand);
+            RuaCommand = new Command(ExecuteRuaCommand);
+            HibridoCommand = new Command(ExecuteHibridoCommand);
+            Type = MapType.Street;
+
+            //double lat = -23.5329467;
+            //double longi = -46.53525352;
+            //var pin = new Pin();
+            //pin.Address = "abc";
+            //pin.Label = "Test Pin 1";
+            //pin.Type = PinType.SavedPin;
+            //pin.Position = new Position(lat, longi);
+
+            //map.Pins.Add(pin);
+            //map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(lat, longi), Distance.FromKilometers(1)));
+        }
+
+        private void ExecuteHibridoCommand(object obj)
+        {
+            Type = MapType.Hybrid;
+        }
+
+        private void ExecuteRuaCommand(object obj)
+        {
+            Type = MapType.Street;
+        }
+
+        private void ExecuteSateliteCommand(object obj)
+        {
+            Type = MapType.Satellite;
+        }
+    }
+}
---
+++ mapsIntegration/mapsIntegration/Views/mapsIntegrationPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms; using Xamarin.Forms.Maps; using mapsIntegration.ViewModels;  namespace mapsIntegration.Views {     public partial class mapsIntegrationPage : ContentPage     {         public mapsIntegrationPage()         {
+            double lat = -23.5329467;
+            double longi = -46.53525352;
+
+            InitializeComponent();             BindingContext = new mapsIntegrationViewModel();
+
+            var pin = new Pin();
+            pin.Address = "abc";
+            pin.Label = "Test Pin 1";
+            pin.Type = PinType.SavedPin;
+            pin.Position = new Position(lat, longi);
+
+            var pin2 = new Pin();
+            pin2.Address = "def";
+            pin2.Label = "Test Pin 2";
+            pin2.Type = PinType.SavedPin;
+            pin2.Position = new Position(-23.53546484, -46.52555466);
+
+            var pin3 = new Pin();
+            pin3.Address = "ghi";
+            pin3.Label = "Test Pin 3";
+            pin3.Type = PinType.SavedPin;
+            pin3.Position = new Position(-23.53215979, -46.53023243);
+
+            MyMap.Pins.Add(pin);
+            MyMap.Pins.Add(pin2);
+            MyMap.Pins.Add(pin3);
+
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(lat, longi), Distance.FromKilometers(1)));
+        }     } }  
